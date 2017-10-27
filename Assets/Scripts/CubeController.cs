bdfef2d220f39d29ca02e3e1c988e224c672d591
Ira Fay <ira@faygames.com>
2017-10-27 14:17:35 -0400

    Part 1

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown () {
+		// turn the previously clicked cube white
+		if (GameController.activeCube != null) {
+			GameController.activeCube.GetComponent<Renderer> ().material.color = Color.white;
+		}
+
+		// now turn the currently clicked cube red
+		gameObject.GetComponent<Renderer> ().material.color = Color.red;
+		GameController.activeCube = gameObject;
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public GameObject cubePrefab;
+	Vector3 cubePosition;
+	public static GameObject activeCube;
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < 16; i++) {
+			cubePosition = new Vector3 (i*2, 0, 0);
+			Instantiate (cubePrefab, cubePosition, Quaternion.identity);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
